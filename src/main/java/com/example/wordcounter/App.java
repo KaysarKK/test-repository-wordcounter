1e3c5fcde585aa49b7a6334b14b014274a49bce5
Kaisar Kassenov <kassenovkaysar@gmail.com>
2018-01-26 10:22:18 +0600

    Update App.java

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -1,13 +1,27 @@
 package com.example.wordcounter;
+import java.lang.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println("Word Count Testing App!");
+        int a = wordcount("Hello Guys, how are you?");
+        System.out.println(a);
     }
     
     public static int wordcount(String s) {
-        return 0;
+    		int i = 0;
+    		String a = s;
+    		int num = 0;
+    		int l = a.length();
+    		char[] b = a.toCharArray();
+    		while (i < l ) {
+    			if (b[i] == ' ') {
+    				num++;
+    			}
+    			i++;
+    		}
+        return num + 1;
     }
 }
